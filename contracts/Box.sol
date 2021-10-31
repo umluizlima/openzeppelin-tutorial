b7a43cad6e7853d7764c890f60e8b5adf991e94e
Luiz Lima <umluizlima@gmail.com>
2021-10-31 12:04:16 -0300

    Use Auth on Box

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,19 +2,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 contract Box {
     uint256 private _value;
+    Auth private _auth;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    // Stores a new value in the contract
+    constructor() {
+        _auth = new Auth(msg.sender);
+    }
+
     function store(uint256 value) public {
+        // Require that the caller is registered as an administrator in Auth
+        require(_auth.isAdministrator(msg.sender), "Unauthorized");
+
         _value = value;
         emit ValueChanged(value);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
         return _value;
     }
