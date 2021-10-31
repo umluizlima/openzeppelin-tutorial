4afe5c2082f2294bbd58b38fbb8897ddbaa85202
Luiz Lima <umluizlima@gmail.com>
2021-10-31 12:19:50 -0300

    Add index script

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// scripts/index.js
+async function main () {
+  // Our code will go here
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
