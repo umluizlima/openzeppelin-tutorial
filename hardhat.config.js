961c804f9fcb309fe37afd351838a91e9091b1fb
Luiz Lima <umluizlima@gmail.com>
2021-10-31 15:14:28 -0300

    Fetch rinkeby secrets from json

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,17 @@
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-truffle5");
 
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
