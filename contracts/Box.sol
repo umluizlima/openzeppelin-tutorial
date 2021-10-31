c0ac6ef1b8aef6255fb5deba33d9c91e3fa5d90d
Luiz Lima <umluizlima@gmail.com>
2021-10-31 12:08:16 -0300

    Use openzeppelin's Ownable instead of custom Auth

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,23 +2,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private _value;
-    Auth private _auth;
 
     event ValueChanged(uint256 value);
 
-    constructor() {
-        _auth = new Auth(msg.sender);
-    }
-
-    function store(uint256 value) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
