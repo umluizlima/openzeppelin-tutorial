a3712ce128063e53adb2cb1fd581c900f29d54d6
Luiz Lima <umluizlima@gmail.com>
2021-10-31 12:22:50 -0300

    Log current Box value

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,16 @@ async function main () {
   // Retrieve accounts from the local node
   const accounts = await ethers.provider.listAccounts();
   console.log(accounts);
+
+  // Set up an ethers contract, representing our deployed Box instance
+  // Replace address with the locally deployed contract address
+  const address = '0x123456';
+  const Box = await ethers.getContractFactory('Box');
+  const box = await Box.attach(address);
+
+  // Call the retrieve() function of the deployed Box contract
+  const value = await box.retrieve();
+  console.log('Box value is', value.toString());
 }
 
 main()
